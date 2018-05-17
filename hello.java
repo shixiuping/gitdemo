0ebc37d230c623b69dba33c0e7b49ccc0f6a38da
shixiuping <1073550520@qq.com>
2018-05-17 10:02:43 +0800

    修改文件

--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@ public class HelloWorld
   public static void main(String[] args)  
   {   
     System.out.println("Hello,World!");
+	System.out.println("Hello,girl!");
 	System.out.println("Hello,World!Hello,World!Hello,World!");
   }  
 }
