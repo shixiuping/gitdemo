4ebfb8a51b77440f18d6b359c661ee1f2f538526
shixiuping <39331048+shixiuping@users.noreply.github.com>
2018-05-17 09:50:15 +0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -4,5 +4,6 @@ public class HelloWorld
   {   
     System.out.println("Hello,World!");
 	System.out.println("Hello,World!Hello,World!Hello,World!");
+	  System.out.println("Hello,World!Hello,World!Hello,World!Hello,World!Hello,World!");
   }  
 }
